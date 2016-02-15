6bc7e7a640b94eae4e8e56dcea9641e986cb8ff0
Javier Garmón <jgarmon@inevio.com>
2016-02-15 08:24:12 +0100

    Improved sync

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var chokidar = require('chokidar');
 var colors   = require('colors');
 var execFile = require('child_process').execFile;
 var notifier = require('node-notifier');
+var path     = require('path');
 
 // Process args
 const SOURCE  = process.argv[ 2 ];
@@ -58,14 +59,14 @@ var rsync = function( callback ){
 
   if( firstSync ){
     firstSync = false;
-    notifier.notify( { 'title': 'Convector', 'message': 'First sync, it can take a while...' }, function(){} );
+    notifier.notify( { 'title': 'Convector', 'message': 'First sync, it can take a while...', icon: path.join(__dirname, 'img', 'stop.png') }, function(){} );
     console.log( 'First sync, it can take a while...'.yellow );
   }else{
-    notifier.notify( { 'title': 'Convector', 'message': 'Syncing...' }, function(){} );
+    notifier.notify( { 'title': 'Convector', 'message': 'Syncing...', icon: path.join(__dirname, 'img', 'stop.png') }, function(){} );
     console.log( 'Syncing...'.yellow );
   }
 
-  execFile( 'rsync', [ '-avrP', '--exclude=".*"', SOURCE, DESTINY ], { cwd : process.cwd, maxBuffer : 1024 * 1024 * 1024 }, function( error, stdout, stderr ){
+  execFile( 'rsync', [ '-vrcP', '--exclude=".*"', SOURCE, DESTINY ], { cwd : process.cwd, maxBuffer : 1024 * 1024 * 1024 }, function( error, stdout, stderr ){
 
     if( stderr ){
       console.log( stderr.red );
@@ -74,7 +75,7 @@ var rsync = function( callback ){
 
     var count = ( stdout.match( /to-check/g ) || [] ).length;
 
-    notifier.notify( { 'title': 'Convector', 'message': 'Uploaded ' + count + ( count === 1 ? ' file' : ' files' ) }, function(){} );
+    notifier.notify( { 'title': 'Convector', 'message': 'Uploaded ' + count + ( count === 1 ? ' file' : ' files' ), icon: path.join(__dirname, 'img', 'go.png'), sound: true }, function(){} );
     console.log( 'Uploaded ' + count + ( count === 1 ? ' file' : ' files' ) );
 
     callback();
